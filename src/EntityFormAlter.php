010d2b8a88cae3ac6e31c2b2b7bf6b0932a77cd5
Carlos Busto Capeáns <cbccharlie@gmail.com>
2017-04-24 17:16:40 +0200

    Remove duplicate js libraries inclusion.

--- src/EntityFormAlter.php
+++ src/EntityFormAlter.php
@@ -17,12 +17,8 @@ class EntityFormAlter {
     /** @var \Drupal\Core\Entity\Entity $entity */
     $entity = $form_state->getFormObject()->getEntity();
     $url = $entity->toUrl('canonical', ['absolute' => TRUE])->toString();
-
-    $element['#attached']['drupalSettings']['siteimprove']['token'] = \Drupal::service('siteimprove.utils')->getSiteimproveToken();
     $element['#attached']['drupalSettings']['siteimprove']['input'] = \Drupal::service('siteimprove.utils')->getSiteimproveSettings($url, 'input');
     $element['#attached']['drupalSettings']['siteimprove']['recheck'] = \Drupal::service('siteimprove.utils')->getSiteimproveSettings($url, 'recheck', FALSE);
-    $element['#attached']['library'][] = \Drupal::service('siteimprove.utils')->getSiteimproveOverlayLibrary();
-    $element['#attached']['library'][] = \Drupal::service('siteimprove.utils')->getSiteimproveLibrary();
 
     return $element;
   }
